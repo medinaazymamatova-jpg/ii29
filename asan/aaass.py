6b1d6579dc39830cbbc6d3afcf733efd8b7d9fba
medinaazymamatova-jpg <medinaazymamatova@gmail.com>
2026-01-08 15:29:04 +0600

    asan

---
+++ asan/aaass.py
@@ -0,0 +1,11 @@
+2. Ноутбук => GitHub
+git init
+git remote add origin ...
+git remote -v
+git add .
+git commit -m 'vhjcvadj'
+git push origin main
+
+.gitignore
+
+git pull
---
+++ asan/views.py
@@ -0,0 +1,9 @@
+1. Github => ноутбукка
+
+git clone ...
+git status (кызылб жашыл)
+git add .
+git commit -m 'коммит'
+git push
+
+.gitignore
